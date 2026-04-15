055a774c5f94a9a9c6241b8358c40241c0fa6089
Gregory D. <157378532+gp06drap@users.noreply.github.com>
2026-04-15 13:54:10 +0000

    little change

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-
+console.log(location.hostname);
+document.querySelector("head").innerHTML="<title>"+location.pathname.substring(1)+"</title><link rel='stylesheet' href='"+location.hostname+"/style.css'>";
